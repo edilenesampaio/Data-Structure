74a5582995b5335952e93cbcb6418ec7c823b61d
Edilene Sampaio <edilenesampaio7@gmail.com>
2023-12-01 14:23:43 -0600

    villagers hobby

--- villager_data.py
+++ villager_data.py
@@ -7,10 +7,10 @@ def all_species(filename):
         - set[str]: a set of strings
     """
     
+    unique_species = []
     file = open(filename)
     for line in file:
         species = line.rstrip().split("|")[1]
-        unique_species = []
         unique_species.append(species)
         print()
     unique_species = set(unique_species)
@@ -28,7 +28,7 @@ def all_species(filename):
 
  
 
-all_species("villagers.csv")
+# all_species("villagers.csv")
 
 def get_villagers_by_species(filename, search_string="All"):
     """Return a list of villagers' names by species.
@@ -41,11 +41,25 @@ def get_villagers_by_species(filename, search_string="All"):
         - list[str]: a list of names
     """
 
-    villagers = []
+    
 
     # TODO: replace this with your code
 
+    villagers = []
+    file = open(filename)
+
+    for line in file:
+        if search_string == "All":
+            villagers.append(line.rstrip().split("|")[0])
+        elif line.rstrip().split("|")[1] == search_string:
+            villagers.append(line.rstrip().split("|")[0])
+    file.close()
+    print(sorted(villagers))
     return sorted(villagers)
+
+
+# get_villagers_by_species("villagers.csv", "Anteater")
+
 
 
 def all_names_by_hobby(filename):
@@ -59,9 +73,36 @@ def all_names_by_hobby(filename):
     """
 
     # TODO: replace this with your code
+    fitness = []
+    nature = []
+    education = []
+    music = []
+    fashion = []
+    play = []
+    names = []
 
-    return []
+    data = open(filename)
+    for line in data:
+        if line.rstrip().split("|")[3]== "Fitness":
+            fitness = fitness.append(line.rstrip().split("|")[0])
+        elif line.rstrip().split("|")[3]== "Nature":
+            nature = nature.append(line.rstrip().split("|")[0])
+        elif line.rstrip().split("|")[3]== "Education":
+            education = education.append(line.rstrip().split("|")[0])
+        elif line.rstrip().split("|")[3]== "Music":
+            music = music.append(line.rstrip().split("|")[0])
+        elif line.rstrip().split("|")[3]== "Fashion":
+            fashion = fashion.append(line.rstrip().split("|")[0])
+        elif line.rstrip().split("|")[3]== "Play":
+            play = play.append(line.rstrip().split("|")[0])
+        print("hello")
+    data.close()
 
+    names.append(fitness)
+    print(names)
+    # return []
+
+all_names_by_hobby("villagers.csv")
 
 def all_data(filename):
     """Return all the data in a file.
